d0734b0ca288d359c550b5cc0bd8a865e72f0289
grass19900325 <grass19900325@gmail.com>
2015-05-14 21:23:20 +0300

    authentication interceptors

---
+++ src/app/modules/authentication/interceptors/auth.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from 'environments/environment';
+import { LocalStorageService } from '../services/local-storage.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private localStorageService: LocalStorageService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const isApiUrl = request.url.startsWith(environment.apiUrl);
+    const accessToken = this.localStorageService.getAccessToken();
+
+    if (accessToken && isApiUrl) {
+      request = request.clone({
+        setHeaders: { Authorization: `Bearer ${accessToken}` },
+      });
+    }
+
+    return next.handle(request);
+  }
+}
